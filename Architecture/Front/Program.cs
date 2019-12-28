64877d8fe26ecabce7a92110a4e881883d4e4708
lalo <lalo_er@hotmail.com>
2019-12-28 01:36:38 -0600

    Initial solution without dependencies

---
+++ Architecture/Front/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Front
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
